5535083970424da2c381d30a1725488e72e24a0e
mitalisoti <smitali.07@gmail.com>
2024-10-21 15:48:42 +0200

    added

--- test/com/example/ValidateISBN.java
+++ test/com/example/ValidateISBN.java
@@ -1,4 +1,40 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ValidateISBN {
+    public boolean checkISBN(String isbn) {
+        int total = 0;
+        for(int i = 0; i < 10; i++) {
+            // total += isbn.charAt(i) - '0';
+            total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+
+        }
+        if(total % 11 == 0){
+            return true;
+        } else{
+            return false;
+        }
+    }
+    public boolean checkISBN2(String isbn) {
+        int total = 0;
+        for(int i = 0; i < 13; i++) {
+            int number = Integer.parseInt(isbn.substring(i, i + 1));
+            total += (i%2 == 0) ? number : number * 3;
+
+        }
+        int sum = total/ 10;
+        if(total % 10 == 0){
+            return true;
+        }
+        return false;
+
+
+    }
 }
+
+
+
+
--- test/com/example/ValidateISBNTest.java
+++ test/com/example/ValidateISBNTest.java
@@ -1,4 +1,36 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ValidateISBNTest {
+
+    @Test
+    public void checkValidateISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("0735211299");
+        assertTrue(result);
+    }
+    @Test
+    public void checkInValidateISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("073e5211299");
+        assertFalse(result);
+    }
+    @Test
+    public void checkValidateISBN2() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN2("978030406157");
+        assertTrue(result);
+    }
+    @Test
+    public void checkInvalidISBN2(){
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN2("123456789012f");
+                assertFalse(result);
+    }
+
+
 }
